4637fd949c5a0c8688f7a1e875206a7656f5c7c5
RamnikDhoot <99842947+RamnikDhoot@users.noreply.github.com>
2023-12-04 17:04:32 +0000

    Created print menu component with option for printing in separate popup window

--- Notes app prototype 2/src/main/java/com/example/NotesApplication.java
+++ Notes app prototype 2/src/main/java/com/example/NotesApplication.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,6 +44,13 @@ public class NotesApplication {
 
         // Create menus
         JMenu fileMenu = new JMenu("File");
+        JMenuItem printMenuItem = new JMenuItem("Print");
+
+         // Add action listener to the "Print" menu item
+         printMenuItem.addActionListener(e -> showPrintMenu());
+        
+        fileMenu.add(printMenuItem);
+
         JMenu editMenu = new JMenu("Edit");
         JMenu viewMenu = new JMenu("View");
         JMenu homeMenu = new JMenu("Home");
@@ -59,7 +68,7 @@ public class NotesApplication {
         exitMenuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.exit(0);
+                System.exit(0); //Closes program when exit is clicked
             }
         });
         fileMenu.add(exitMenuItem);
@@ -85,6 +94,41 @@ public class NotesApplication {
 
     private void addNoteToList(String note) {
         notes.add(note);
+    }
+
+     public void showPrintMenu() {
+        JDialog printDialog = new JDialog(frame, "Print Options", true);
+        printDialog.setSize(200, 100);
+        printDialog.setLayout(new FlowLayout());
+
+        JButton printToPrinterButton = new JButton("Print to Printer");
+        JButton printPreviewButton = new JButton("Print Preview");
+
+        // Add action listeners to the print options
+        printToPrinterButton.addActionListener(e -> printNoteToPrinter());
+        printPreviewButton.addActionListener(e -> showPrintPreview());
+
+        // Add buttons to the dialog
+        printDialog.add(printToPrinterButton);
+        printDialog.add(printPreviewButton);
+
+        // Set dialog location relative to the main frame
+        printDialog.setLocationRelativeTo(frame);
+
+        // Make the dialog visible
+        printDialog.setVisible(true);
+    }
+
+    private void printNoteToPrinter() {
+        // Implement the logic to print the note to a printer
+        // You can use PrinterJob and NotePrintable as in the previous examples
+        System.out.println("Printing to Printer");
+    }
+
+    private void showPrintPreview() {
+        // Implement the logic to show print preview
+        // You can customize this based on your needs
+        System.out.println("Show Print Preview");
     }
 
     public static void main(String[] args) {
