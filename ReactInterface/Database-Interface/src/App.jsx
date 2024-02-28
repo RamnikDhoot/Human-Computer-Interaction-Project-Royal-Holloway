88b3a2137e12d9250a34bda440568c37df0f014b
RamnikDhoot <99842947+RamnikDhoot@users.noreply.github.com>
2024-02-28 17:53:38 +0000

    separated sidebar from navbar and created home.jsx

--- ReactInterface/Database-Interface/src/App.jsx
+++ ReactInterface/Database-Interface/src/App.jsx
@@ -1,17 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar/NavBar.jsx'
-import Footer from './components/Footer.jsx';
-import CoverPage from './pages/CoverPage.jsx';
-import Home from './pages/Home.jsx';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import Footer from "./components/Footer.jsx";
+import CoverPage from "./pages/CoverPage.jsx";
+import Home from "./pages/Home.jsx";
+import "./App.css";
+import SideBar from "./components/SideBar.jsx";
 
 function App() {
   return (
     <>
-      <Home/>
+      
     </>
   );
 }
 
-export default App
+export default App;
+
+{/* <NavBar />
+      <div className="container-fluid">
+        <SideBar />
+
+        <Home />
+        
+      </div> */}
