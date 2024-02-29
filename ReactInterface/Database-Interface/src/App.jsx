08b01f1a3f6fe09aa66308d9c7e89e4bcd84a24e
RamnikDhoot <99842947+RamnikDhoot@users.noreply.github.com>
2024-02-29 16:34:45 +0000

    Created Signup page in React

--- ReactInterface/Database-Interface/src/App.jsx
+++ ReactInterface/Database-Interface/src/App.jsx
@@ -1,18 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 import NavBar from "./components/NavBar/NavBar.jsx";
 import Footer from "./components/Footer.jsx";
 import CoverPage from "./pages/CoverPage.jsx";
 import Home from "./pages/Home.jsx";
-import "./App.css";
 import SideBar from "./components/SideBar.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import SaleData from "./pages/Sale data.jsx";
 import SignIn from "./pages/Login pages/SignIn.jsx";
 import TwoFactorAuth from "./pages/Login pages/2FA.jsx";
+import Signup from "./pages/Login pages/Signup.jsx";
 
 function App() {
   return (
-    <TwoFactorAuth/>
+    <Signup/>
       
   );
 }
